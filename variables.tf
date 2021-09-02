2d4b1f3b381be2e669a8b719b989454b2a514635
cassyomailard <53494260+cassyomailard@users.noreply.github.com>
2021-09-01 23:21:49 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,17 @@
 variable "amis" {
     default = {
-        "ubuntu20" = "ami-09e67e426f25ce0d7"
+        "ubuntu20" = "ami-09e67e426f25ce0d7" #Substituir pela ami desejada
     }
 }
 
 variable "instance_type" {
     default = {
-        "micro" = "t2.micro"
+        "micro" = "t2.micro" #Substituir pelo tipo de instancia desejada
+    }
+}
+
+variable "instance_type" {
+    default = {
+        "micro" = "t2.micro" #Substituir pelo tipo de instancia desejada
     }
 }
